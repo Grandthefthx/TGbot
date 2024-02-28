ba023ce05eb1ce77a72d1bfe8729746600578623
Granin Ilia <graninilan@gmail.com>
2024-02-28 20:52:46 +0500

    another some changes

--- some_new_file.py
+++ some_new_file.py
@@ -1,4 +1,11 @@
-print("My first commit")
-print("My new coom")
-print("1moreCOmmit")
-print("1morrrrrrr")
+import requests
+
+
+api_url = 'http://numbersapi.com/43'
+
+response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)    # При другом коде ответа выводим этот код
