c57e0b66d737c1d2ec71921bdf632a89c1af3e1b
Granin Ilia <graninilan@gmail.com>
2024-03-01 10:51:27 +0500

    2nd

--- some_new_file.py
+++ some_new_file.py
@@ -1,11 +1,25 @@
-import requests
-
-
-api_url = 'http://numbersapi.com/43'
-
-response = requests.get(api_url)   # Отправляем GET-запрос и сохраняем ответ в переменной response
-
-if response.status_code == 200:    # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
-    print(response.text)
-else:
-    print(response.status_code)    # При другом коде ответа выводим этот код
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+BOT_TOKEN = '6900390654:AAExoyCJ1anMaZxJHW9C0ZktyLD-xGiSYnI'
+# Создаем объекты бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer(
+        'Напиши мне что-нибудь и в ответ '
+        'я пришлю тебе твое сообщение')
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+if __name__ == '__main__':
+    dp.run_polling(bot)
